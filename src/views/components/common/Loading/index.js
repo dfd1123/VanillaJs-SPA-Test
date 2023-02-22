2067ed33b1461cfa0c17eed1889c66f655b9c814
김종완 <42544793+dfd1123@users.noreply.github.com>
2023-02-23 00:05:22 +0900

    Loading 컴포넌트 추가

---
+++ src/views/components/common/Loading/index.js
@@ -0,0 +1,14 @@
+import "./style.scss";
+import Component from "@/core/Component";
+
+export default class Loading extends Component {
+    templete(){
+        return `
+            <div class="loader">
+                <svg class="circular-loader" viewBox="25 25 50 50">
+                    <circle class="loader-path" cx="50" cy="50" r="20"></circle>
+                </svg>
+            </div>
+        `;
+    }
+}
